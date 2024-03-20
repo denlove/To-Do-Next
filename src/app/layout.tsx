12e1de3660beb9318d5b7efc5c7c3c1ee40065e3
denlove <bronzovdenis59@gmail.com>
2024-03-20 18:08:24 +0300

    TDN-2: create registry component for styled-components module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
-import { ParamChildren } from '@/types/interfaces'
+import StyledComponentsRegistry from '@/lib/registry'
+import Header from '@/models/Header'
+import { IParamChildren } from '@/types/interfaces'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'To-Do | Home',
 }
 
-export default function RootLayout({ children }: Readonly<ParamChildren>) {
+export default function RootLayout({ children }: Readonly<IParamChildren>) {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <body>
+                <StyledComponentsRegistry>
+                    <Header />
+                    {children}
+                </StyledComponentsRegistry>
+            </body>
         </html>
     )
 }
