033621ebcc82a6500792da9125b0a3c988e0e3a2
denlove <bronzovdenis59@gmail.com>
2024-03-20 18:04:32 +0300

    TDN-2: update interface of children parameters

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,3 +1,10 @@
-export interface ParamChildren {
+export interface IParamChildren {
     children: React.ReactNode
+}
+
+export interface ILinkedImage {
+    id?: number
+    href: string
+    src: any
+    alt: string
 }
