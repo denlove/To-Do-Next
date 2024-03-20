42f73872eb6b991f4d2eea13759c2f97c13b3fc9
denlove <bronzovdenis59@gmail.com>
2024-03-20 17:54:51 +0300

    TDN-2: edit config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const nextConfig = {
+    compiler: {
+        styledComponents: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
 
 export default nextConfig
