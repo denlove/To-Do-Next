8affedcc178c8e90e8da91854f7335dc59a146e5
denlove <bronzovdenis59@gmail.com>
2024-03-24 12:45:47 +0300

    TDN-3: add utils architecture layer

---
+++ src/utils/contents.ts
@@ -0,0 +1,8 @@
+// To-Do List mock
+export const TASK_1 = 'Drink coffie'
+export const TASK_2 = 'Hang out with friends'
+export const TASK_3 = 'Do homework'
+
+// Footer
+export const RESERVED_RIGHTS = 'All rights reserved.'
+export const CURRENT_YEAR = '2024'
