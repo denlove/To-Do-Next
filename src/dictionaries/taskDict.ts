e31315c8caf3c017df39f31fdf6724eaf012d7b6
denlove <bronzovdenis59@gmail.com>
2024-03-24 12:48:53 +0300

    TDN-3: add new and update existing project types and dictionaries

---
+++ src/dictionaries/taskDict.ts
@@ -0,0 +1,8 @@
+import { ITaskInfo } from '@/types/interfaces'
+import { TASK_1, TASK_2, TASK_3 } from '@/utils/contents'
+
+export const taskDict: Array<ITaskInfo> = [
+    { id: 1, content: TASK_1, isCheck: undefined },
+    { id: 2, content: TASK_2, isCheck: undefined },
+    { id: 3, content: TASK_3, isCheck: undefined },
+]
