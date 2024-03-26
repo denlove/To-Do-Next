7002edab6a9b6d02c7b874d2cfb3768722806af0
denlove <bronzovdenis59@gmail.com>
2024-03-26 16:36:10 +0300

    TDN-4: add new dictionaries for ListControls and TaskControls

---
+++ src/dictionaries/taskControlsDict.ts
@@ -0,0 +1,6 @@
+import { ITaskControls } from '@/types/interfaces'
+
+export const taskControlsDict: Array<ITaskControls> = [
+    { id: 1, act: () => {}, variant: 'image-like' },
+    { id: 2, act: () => {}, variant: 'image-like' },
+]
