83a3a4a147192673cc017cf9817150b4796b87e8
denlove <bronzovdenis59@gmail.com>
2024-03-26 16:43:00 +0300

    TDN-4: add Header styles, implement app Provider

--- src/models/Header/Header.tsx
+++ src/models/Header/Header.tsx
@@ -1,11 +1,15 @@
 import Logo from '@/components/Logo/Logo'
 import Navbar from '@/components/Navbar/Navbar'
+import GeneralWrapper from '@/wrappers/GeneralWrapper'
+import styles from './Header.module.scss'
 
 const Header = () => {
     return (
-        <header>
-            <Logo />
-            <Navbar />
+        <header id={styles.header}>
+            <GeneralWrapper>
+                <Logo />
+                <Navbar />
+            </GeneralWrapper>
         </header>
     )
 }
