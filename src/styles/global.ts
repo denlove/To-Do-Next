355517e3dfde00c002a60231d682ce9b060fe50c
denlove <bronzovdenis59@gmail.com>
2024-03-30 19:10:21 +0300

    TDN-5: add float font mixin, edit displayFlex template, add new css variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,16 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyles = createGlobalStyle`
     :root {
         --max-content: 1280px;
-        --header-height: 4.375rem
+        --header-height: 4.375rem;
+        --footer-height: 3.125rem;
+
+        // fonts sizes
+        --fs-16: 1rem;
+        --fs-20: 1.25rem;
+        --fs-48: 3rem;
+
+        // calcs
+        --task-area-height: calc(100vh - var(--header-height) - var(--footer-height));
     }
     * {
         border: none;
@@ -11,8 +20,13 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
         outline: none;
     }
+
     body {
-        background-color: hsla(0, 0%, 30%);
+        background-color: hsla(0, 0%, 80%);
+    }
+
+    a {
+        text-decoration: none;
     }
 `
 export default GlobalStyles
--- src/styles/templates.ts
+++ src/styles/templates.ts
@@ -1,12 +1,21 @@
-import { IDefAllProperties } from '@/types/interfacesBP'
+import { FlexStyles } from '@/types/types'
 import { css } from 'styled-components'
 
-interface IDisplayFlex extends IDefAllProperties<string> {}
+type DisplayFlexType = Partial<{
+    [k in FlexStyles]: string
+}>
 
-export const displayFlex = ({ jc, ai }: IDisplayFlex) => {
+export const displayFlex = ({
+    jc = 'center',
+    ai = 'center',
+    fd = 'row',
+    gp = '0',
+}: DisplayFlexType) => {
     return css`
         display: flex;
+        flex-direction: ${fd};
         justify-content: ${jc};
         align-items: ${ai};
+        gap: ${gp};
     `
 }
