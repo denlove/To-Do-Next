bd0d281100f9866b5e83eb0a1e0faa145a9b7639
denlove <bronzovdenis59@gmail.com>
2024-04-03 22:22:52 +0300

    TDN-8: add new styles for ListHeader component

--- src/components/ListHeader/ListHeader.tsx
+++ src/components/ListHeader/ListHeader.tsx
@@ -1,17 +1,19 @@
+'use client'
+
 import c from 'classnames'
 import Heading from '@/ui/Heading/Heading'
 import Paragraph from '@/ui/Paragraph/Paragraph'
-import styles from './ListHeader.module.scss'
 import { IClassName } from '@/types/interfacesBP'
+import { HeadingGroup, StyledArticle } from './ListHeader.styled'
 
 const ListHeader = ({ className }: Partial<IClassName>) => {
     return (
-        <article className={c(styles['list-header'], className)}>
-            <hgroup className={styles['list-header__hgroup']}>
+        <StyledArticle className={className}>
+            <HeadingGroup>
                 <Heading elem='h1' content='Gorgeous To-Do List' />
                 <Paragraph content='Welcome, friend' />
-            </hgroup>
-        </article>
+            </HeadingGroup>
+        </StyledArticle>
     )
 }
 
