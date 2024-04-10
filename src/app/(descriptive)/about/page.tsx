7e984743bf4a18d153500605bec7c2baeb6a5767
denlove <bronzovdenis59@gmail.com>
2024-04-10 11:53:16 +0300

    TDN-9: edit files in type-ckeck build pattern

--- src/app/(descriptive)/about/page.tsx
+++ src/app/(descriptive)/about/page.tsx
@@ -1,3 +1,3 @@
 export default function AboutPage() {
-    return <div>About</div>
+    return <h1>About</h1>
 }
--- src/dictionaries/navbarDict.ts
+++ src/dictionaries/navbarDict.ts
@@ -4,7 +4,7 @@ import { ABOUT_ROUTE, HELP_ROUTE, HOME_ROUTE } from '@/utils/routes'
 type NavbarType = Array<INavbarItem>
 
 export const navbarDict: NavbarType = [
-    { id: 1, route: HOME_ROUTE, content: 'Home' },
-    { id: 2, route: ABOUT_ROUTE, content: 'About us' },
-    { id: 3, route: HELP_ROUTE, content: 'Help' },
+    { id: 1, route: HOME_ROUTE, content: 'Home', isActive: false },
+    { id: 2, route: ABOUT_ROUTE, content: 'About us', isActive: false },
+    { id: 3, route: HELP_ROUTE, content: 'Help', isActive: false },
 ]
