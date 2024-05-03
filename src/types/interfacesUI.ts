dd8e667fe41467e90c742e3ceb97f754a924d73c
denlove <bronzovdenis59@gmail.com>
2024-05-03 23:52:30 +0300

    TDN-14: add new types, global styles, remake queries

--- src/types/interfacesUI.ts
+++ src/types/interfacesUI.ts
@@ -1,18 +1,23 @@
+import { HTMLProps } from 'react'
 import { ITaskInfo } from './interfaces'
-import { ButtonVariants } from './types'
+import { ButtonVariants, TextAreaVariants } from './types'
 
 // interfaces for ui directory
 
 export interface IInput
     extends Partial<Pick<ITaskInfo, 'isCheck'>>,
-        React.HTMLProps<HTMLInputElement> {
+        HTMLProps<HTMLInputElement> {
     type: 'button' | 'checkbox' | 'text'
     value?: string
 }
 
-export interface IButton extends React.HTMLProps<HTMLButtonElement> {
+export interface IButton extends HTMLProps<HTMLButtonElement> {
     children?: React.ReactNode
     variant: ButtonVariants
     type?: 'button'
     text?: string
+}
+
+export interface ITextArea extends HTMLProps<HTMLTextAreaElement> {
+    variant: TextAreaVariants
 }
--- src/utils/contents.ts
+++ src/utils/contents.ts
@@ -6,3 +6,9 @@ export const TASK_3 = 'Do homework'
 // Footer
 export const RESERVED_RIGHTS = 'All rights reserved.'
 export const CURRENT_YEAR = '2024'
+
+// Queries point
+export const MOBILE = 'MOBILE'
+export const LARGE_MOBILE = 'LARGE_MOBILE'
+export const TABLET = 'TABLET'
+export const DESKTOP = 'DESKTOP'
